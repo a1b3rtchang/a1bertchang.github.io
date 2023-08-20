e6d48585921f49ae582fd2ecd9c21e2ddc48a698
albert chang <alberthchang@berkeley.edu>
2023-08-19 17:09:51 -0700

    revert update

---
+++ src/pages/contact.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+ 
+const Contact: React.FC= () => {
+    return (
+        <div className="page">
+            <h1>
+                Contact
+            </h1>
+            <div>
+                <div>Email: albert@kchang.net</div>
+                <div>Phone: (530)-302-6444</div>
+                <a href="https://www.linkedin.com/in/alberthchang/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+            </div>
+        </div>
+    );
+};
+ 
+export default Contact;
